2786c0948a173e46c388c19192e0d3e336231167
= <=>
2025-04-21 15:33:42 +0200

    format: show/hide UI elements according to the user permissions

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createApp, h } from "vue";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
-import {Can} from "@/Suport/can.js";
+import { Can } from "@/Suport/can.js";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
