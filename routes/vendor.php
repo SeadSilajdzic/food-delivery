591543097d8cd3a50d8b6b477e629d251e0266d5
= <=>
2025-04-21 16:06:12 +0200

    format: category/product and menu item

--- routes/vendor.php
+++ routes/vendor.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Vendor\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix'     => 'vendor',
-    'as'         => 'vendor.',
+    'prefix' => 'vendor',
+    'as' => 'vendor.',
     'middleware' => ['auth'],
 ], function () {
     Route::get('menu', [MenuController::class, 'index'])->name('menu');
