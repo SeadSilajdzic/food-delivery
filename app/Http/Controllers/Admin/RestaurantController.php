760423f553f0413d17ee80a827145679e893e2ea
= <=>
2025-04-21 11:37:11 +0200

    frontend: return list of restaurants

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RestaurantController extends Controller
+{
+    public function index(): Response
+    {
+        Gate::authorize('restaurant.viewAny', Restaurant::class);
+
+        return Inertia::render('Admin/Restaurants/Index', [
+            'restaurants' => Restaurant::query()->with('city', 'owner')->get(),
+        ]);
+    }
+}
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,10 +32,10 @@ const showingNavigationDropdown = ref(false);
                                 class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
                             >
                                 <NavLink
-                                    :href="route('dashboard')"
-                                    :active="route().current('dashboard')"
+                                    :href="route('admin.restaurants.index')"
+                                    :active="route().current('admin.restaurants.index')"
                                 >
-                                    Dashboard
+                                    Restaurants
                                 </NavLink>
                             </div>
                         </div>
